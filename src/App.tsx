27138d8b6be4e79879d5738212f027bc9e9c269c
Johnrebu <johnchemist91@gmail.com>
2025-02-25 15:43:14 +0530

    project structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmployeeDirectory from './components/EmployeeDirectory';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-50">
+      <EmployeeDirectory />
+    </div>
+  );
+}
+
+export default App;
